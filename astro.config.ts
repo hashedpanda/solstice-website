5c35d2f2d4e80b7862b35692d43ab9f74dee1cc6
Ashe <172499876+hashedpanda@users.noreply.github.com>
2024-11-02 01:28:37 +0100

    chore: add Vercel adapter for Astro

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), icon()]
+	integrations: [mdx(), icon()],
+	output: "server",
+	adapter: vercel()
 });

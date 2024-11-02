148a385c24286c9b931217f93d9c949bc156c8c8
Ashe <172499876+hashedpanda@users.noreply.github.com>
2024-11-02 01:34:30 +0100

    chore: set rendering mode to `hybrid`
    This means that pages will be prerendered unless they explicitly opt out.

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), icon()],
-	output: "server",
+	output: "hybrid",
 	adapter: vercel()
 });
